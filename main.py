b61e2a581abfac4b828e69263be15b86ceb76cf2
hachaeva <hachaeva@hotmail.com>
2025-07-03 20:44:26 -0400

    created bookbot

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 # take a filepath as input and return the contents of the file as a string
 
 from stats import get_num_words
+from stats import get_num_characters
+from stats import character_count_dict
+from stats import sort_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
+def print_report(list,path,word_count):
+    print ("============ BOOKBOT ============")
+    print (f"Analyzing book found at {path}...")
+    print ("----------- Word Count ----------")
+    print (f"Found {word_count} total words")
+    print ("--------- Character Count -------")
+    for character in list:
+        if (character["char"].isalpha()):
+            print (f"{character["char"]}: {character["num"]}")
+    print ("============= END ===============")
+    return
+
+
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    path = "./books/frankenstein.txt"
+    book_text = get_book_text(path)
     num_words = get_num_words(book_text)
-    print (f"{num_words} words found in the document")
+    num_characters = get_num_characters(book_text)
+    count_dict = character_count_dict(book_text)
+    sorted_list = sort_count(count_dict)
+    print_report(sorted_list,path,num_words)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,33 @@
 def get_num_words(text):
     word_count = len(text.split())
     return word_count
+
+def get_num_characters(text):
+    words = text.split()
+    character_count = 0
+    for word in words:
+        character_count += len(word)
+    return character_count
+
+def character_count_dict(text):
+    count_dict = {}
+    for character in text.lower():
+        if character in count_dict.keys():
+            count_dict[character] += 1
+        else:
+            count_dict[character] = 1
+    return count_dict
+
+def sort_on(items):
+    return items["num"]
+
+def sort_count(count_dict):
+    sorted_list = []
+    new_dict = {}
+    for element in count_dict:
+        letter = {}
+        letter["char"] = element
+        letter["num"] = count_dict[element]
+        sorted_list.append(letter)
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
